b21228c66d195f665956493ab20e694fb13abf73
Yy <yyned2501@vip.qq.com>
2025-02-28 16:40:17 +0800

    添加控制台输出

--- app/libs/logs.py
+++ app/libs/logs.py
@@ -11,4 +11,6 @@ file_handler = RotatingFileHandler(
     "logs/main.log", maxBytes=10 * 1024 * 1024, backupCount=5
 )
 file_handler.setFormatter(formatter)
+console_handler = logging.StreamHandler()
 logger.addHandler(file_handler)
+logger.addHandler(console_handler)
