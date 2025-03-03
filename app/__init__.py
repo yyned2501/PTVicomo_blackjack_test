daeb03b0a390b2ce7be2c8f4eae85335e07923e6
Yy <yyned2501@vip.qq.com>
2025-03-03 14:58:15 +0800

    bug fix

--- app/__init__.py
+++ app/__init__.py
@@ -29,15 +29,13 @@ class Client(_Client):
                 return await super().invoke(*arg, **kargs)
             except TimeoutError as e:
                 logger.error(traceback.format_exc())
-                asyncio.sleep(1)
                 return await self.invoke(*arg, err=err + 1, **kargs)
-            except pyrogram.errors.exceptions.bad_request_400.QueryIdInvalid:
-                logger.error(traceback.format_exc())
-            except pyrogram.errors.exceptions.bad_request_400.MessageNotModified:
-                logger.error(traceback.format_exc())
+            # except pyrogram.errors.exceptions.bad_request_400.QueryIdInvalid:
+            #     logger.error(traceback.format_exc())
+            # except pyrogram.errors.exceptions.bad_request_400.MessageNotModified:
+            #     logger.error(traceback.format_exc())
             except Exception as e:
                 logger.error(traceback.format_exc())
-                asyncio.sleep(1)
                 return await self.invoke(*arg, err=err + 1, **kargs)
 
 
