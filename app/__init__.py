df336112fc4adf3a0ada9a9873326b2065a822f6
Yy <yyned2501@vip.qq.com>
2025-03-11 10:13:06 +0800

    设置scheduler并发数

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,16 @@ from config import API_ID, API_HASH, BOT_TOKEN, REDIS_HOST, REDIS_PORT, REDIS_DB
 
 
 os.environ["TZ"] = "Asia/Shanghai"
-scheduler = AsyncIOScheduler()
+
+scheduler = AsyncIOScheduler(
+    {
+        "job_defaults": {
+            "max_instances": 5,  # 全局设置最大并发实例数为 5
+            "coalesce": True,  # 可选：合并跳过的任务
+            "misfire_grace_time": 2,  # 可选：设置宽限时间（秒）
+        }
+    }
+)
 
 app: Client = None
 
