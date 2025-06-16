16352fc99a8d251322e9051f1dc7b19b8313ac5d
Yy <yyned2501@vip.qq.com>
2025-06-16 09:42:04 +0800

    优化 info.py 中的用户信息获取逻辑，移除不必要的数据库会话管理，提升代码可读性

--- app/commands/info.py
+++ app/commands/info.py
@@ -25,52 +25,44 @@ UID: {{user.id}}
 @Client.on_message(filters.private & filters.command("info"))
 @auto_delete_message()
 async def secret_info(client: Client, message: Message):
-    async with ASSession() as session:
-        async with session.begin():
-            user = await Users.get_user_from_tgmessage(message)
-            if not user:
-                return await message.reply(USER_BIND_NONE)
-            return await message.reply(Template(INFO_ALL).render(user=user))
+    user = await Users.get_user_from_tgmessage(message)
+    if not user:
+        return await message.reply(USER_BIND_NONE)
+    return await message.reply(Template(INFO_ALL).render(user=user))
 
 
 @Client.on_message(filters.chat(GROUP_ID) & filters.reply & filters.command("info"))
 @auto_delete_message(delete_from_message_immediately=True)
 async def group_reply_info(client: Client, message: Message):
-    async with ASSession() as session:
-        async with session.begin():
-            user = await Users.get_user_from_tgmessage(message)
-            if user and user._class >= 14:
-                reply_user = await Users.get_user_from_tgmessage(
-                    message.reply_to_message
-                )
-                if not reply_user:
-                    await client.send_message(message.from_user.id, "Ta还没有绑定账号")
-                    return
-                await client.send_message(
-                    message.from_user.id,
-                    Template(INFO_ALL).render(user=reply_user),
-                )
+    user = await Users.get_user_from_tgmessage(message)
+    if user and user._class >= 14:
+        reply_user = await Users.get_user_from_tgmessage(message.reply_to_message)
+        if not reply_user:
+            await client.send_message(message.from_user.id, "Ta还没有绑定账号")
+            return
+        await client.send_message(
+            message.from_user.id,
+            Template(INFO_ALL).render(user=reply_user),
+        )
 
 
 @Client.on_message(filters.chat(GROUP_ID) & filters.command("info"))
 @auto_delete_message()
 async def group_info(client: Client, message: Message):
-    async with ASSession() as session:
-        async with session.begin():
-            user = await Users.get_user_from_tgmessage(message)
-            if not user:
-                return await message.reply(USER_BIND_NONE)
-            name = (await client.get_me()).username
-            return await message.reply(
-                f"您有 {user.seedbonus} 象草",
-                reply_markup=InlineKeyboardMarkup(
-                    [
-                        [
-                            InlineKeyboardButton(
-                                "私聊机器人可获取更多信息!",
-                                url=f"https://t.me/{name}",
-                            )
-                        ],
-                    ]
-                ),
-            )
+    user = await Users.get_user_from_tgmessage(message)
+    if not user:
+        return await message.reply(USER_BIND_NONE)
+    name = (await client.get_me()).username
+    return await message.reply(
+        f"您有 {user.seedbonus} 象草",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "私聊机器人可获取更多信息!",
+                        url=f"https://t.me/{name}",
+                    )
+                ],
+            ]
+        ),
+    )
